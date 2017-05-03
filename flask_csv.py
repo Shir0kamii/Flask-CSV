c84e808c05cc2430d13fa9db3d13af6877f9f960
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-05-03 22:10:18 +0200

    Accept additionnal keyword arguments

--- flask_csv.py
+++ flask_csv.py
@@ -3,9 +3,10 @@ from io import BytesIO
 from flask import send_file
 
 
-def send_csv(iterable, filename):
+def send_csv(iterable, filename, **kwargs):
     buf = BytesIO()
     for line in iterable:
         buf.write(str(line).encode("utf-8"))
     buf.seek(0)
-    return send_file(buf, attachment_filename=filename, as_attachment=True)
+    return send_file(buf, attachment_filename=filename, as_attachment=True,
+                     **kwargs)
