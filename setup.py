c1ee66530edef0051ae359501573c84d6d5b1eea
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-05-03 22:09:31 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    fullname = os.path.join(os.path.dirname(__file__), fname)
+    with open(fullname) as fp:
+        content = fp.read()
+    return content
+
+
+setup(
+    name="Flask-CSV",
+    version="0.1.0",
+    url="https://github.com/Shir0kamii/Flask-CSV",
+    author_email="shir0kamii@gmail.com",
+    description="Easily render CSVs within any flask application",
+    long_description=read("README.rst"),
+    download_url="https://github.com/Shir0kamii/Flask-CSV/tags",
+    platforms="any",
+    py_modules=["flask_csv"],
+    install_requires=[
+        "Flask",
+        "csvalidate"
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
