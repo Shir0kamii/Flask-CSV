97cb4d044f30d4f053291ddac800c517aaf7a929
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-05-03 23:18:04 +0200

    Uses csvalidate to validate and modify outgoing data

--- flask_csv.py
+++ flask_csv.py
@@ -1,12 +1,20 @@
-from io import BytesIO
+from io import BytesIO, StringIO
 
+from csvalidate import ValidatedWriter
 from flask import send_file
 
 
-def send_csv(iterable, filename, **kwargs):
-    buf = BytesIO()
+def send_csv(iterable, filename, fields=None, schema=None, delimiter=None,
+             **kwargs):
+    buf = StringIO()
+    writer_cls = ValidatedWriter
+    if schema:
+        writer_cls = ValidatedWriter.from_schema(schema)
+    writer = writer_cls(buf, fields, delimiter=delimiter)
+    writer.writeheader()
     for line in iterable:
-        buf.write(str(line).encode("utf-8"))
+        writer.writerow(line)
     buf.seek(0)
+    buf = BytesIO(buf.read().encode("utf-8"))
     return send_file(buf, attachment_filename=filename, as_attachment=True,
                      **kwargs)
