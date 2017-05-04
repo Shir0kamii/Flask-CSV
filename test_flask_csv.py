b1ab6217e88e5d8ed84c57841f5a1894ad5145e4
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-05-04 02:03:21 +0200

    Add coverage for schema feature

--- test_flask_csv.py
+++ test_flask_csv.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from marshmallow import fields, Schema
 import pytest
 
 from flask_csv import send_csv
+
+
+class IdSchema(Schema):
+    id = fields.Integer()
 
 
 @pytest.fixture
@@ -12,6 +17,17 @@ def client():
     def index():
         return send_csv([{"id": 42, "foo": "bar"}, {"id": 91, "foo": "baz"}],
                         "test.csv", ["id", "foo"])
+    return app.test_client()
+
+
+@pytest.fixture
+def schema_client():
+    app = Flask("test")
+
+    @app.route("/")
+    def index():
+        return send_csv([{"id": 42, "foo": "bar"}, {"id": 91, "foo": "baz"}],
+                        "test.csv", schema=IdSchema())
     return app.test_client()
 
 
@@ -27,5 +43,15 @@ def test_send_csv_content(client):
     expected = b"""id,foo
 42,bar
 91,baz
+""".replace(b'\n', b"\r\n")
+    assert data == expected
+
+
+def test_send_csv_schema(schema_client):
+    response = schema_client.get("/")
+    data = response.data
+    expected = b"""id
+42
+91
 """.replace(b'\n', b"\r\n")
     assert data == expected
