314147a2d89bd530b7fbad5da4238500e902f585
Mathieu <albatros69@users.noreply.github.com>
2018-02-02 09:01:36 +0100

    Cosmetic change

--- flask_csv.py
+++ flask_csv.py
@@ -20,5 +20,4 @@ def send_csv(iterable, filename, fields=None, schema=None, delimiter=',',
     mimetype = 'Content-Type: text/csv; charset='+encoding
 
     return send_file(buf, attachment_filename=filename, as_attachment=True,
-                     mimetype=mimetype,
-                     **kwargs)
+                     mimetype=mimetype, **kwargs)
